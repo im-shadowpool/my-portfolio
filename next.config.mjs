88b45e9476b12deae7eebe1e12ada5d1b6942a2a
im-shadowpool <shadowpoolvs@gmail.com>
2026-09-20 18:28:03 +0530

    added the tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  allowedDevOrigins: ["tips-glen-consulting-victor.trycloudflare.com"],
   async headers() {
     return [
       {
